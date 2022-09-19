97d25a0ff73bf63071d750a1bf69436dd94c6973
Rahul <rahul10kr7@gmail.com>
2022-09-19 23:15:33 +0530

    app initialiaztion

---
+++ todoapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Todo app</h1>
+    </div>
+  );
+}
+
+export default App;
