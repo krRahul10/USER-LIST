80c8f63c184f5948657918651e39252706b30d60
Rahul <rahul10kr7@gmail.com>
2022-09-20 17:42:55 +0530

    server start done

--- server/app.js
+++ server/app.js
@@ -1,15 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
-require('./db/conn')
-
+require("./db/conn");
+const mongoose = require("mongoose");
+const user = require("./models/userSchema");
 const app = express();
+const cors = require("cors");
+
+const router = require("./routes/router");
 
 const port = 8080;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(router);
 
 // const DB= `mongodb+srv://Octopus:octopus@cluster0.fmfphvh.mongodb.net/?retryWrites=true&w=majority`
 // DB="+srv:Todo-List:iamsrkfan@10@cluster0.cr6nb7n.mongodb.net/?retryWrites=true&w=majority"
 // mongodb
-app.listen(port, ()=>{
-    console.log(`server is started port number ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is started port number ${port}`);
+});
---
+++ server/models/userSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  work: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+});
+
+const user = new mongoose.model("users", userSchema);
+
+module.exports = user
