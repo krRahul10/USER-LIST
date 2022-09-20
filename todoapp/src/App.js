6e52f32799da055ef5c7f6232a5142d742fd3376
Rahul <rahul10kr7@gmail.com>
2022-09-20 09:17:26 +0530

    routing done

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -3,12 +3,18 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import { Navbar } from './Components/Navbar';
 import { Home } from './Components/Home';
+import { Register } from './Components/Register';
+import {Route,Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
      <h1>Hello Todo app</h1>
      <Navbar/>
-     <Home/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+     </Routes>
+    
     </div>
   );
 }
